db6b26dfc948b2cdbc0b02a008404ea2dd123b67
JoystonFernands <vijethjoyston@gmail.com>
2024-05-10 13:25:00 +0530

    Add basic gradle setup

---
+++ serverlessapi/src/main/java/com/serverlessjavapoc/serverlessapi/ServerlessapiApplication.java
@@ -0,0 +1,13 @@
+package com.serverlessjavapoc.serverlessapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerlessapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerlessapiApplication.class, args);
+	}
+
+}
---
+++ serverlessapi/src/test/java/com/serverlessjavapoc/serverlessapi/ServerlessapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.serverlessjavapoc.serverlessapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerlessapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
